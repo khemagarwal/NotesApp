5684b087035824c210a856649d30cfc92618af10
coderkhem <khemagarwal1@gmail.com>
2020-04-09 13:06:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/NoteEditorActivity.java
@@ -0,0 +1,60 @@
+package com.example.notesapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashSet;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        EditText editNote = findViewById(R.id.editNoteText);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+        if (noteId != -1){
+            editNote.setText(MainActivity.notes.get(noteId));
+        }
+        else {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
+        }
+
+        editNote.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId, String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.adityakhedekar.khedubaba.notes",
+                        Context.MODE_PRIVATE);
+                //Use hashset instead of objectserializer when order in list doesnt matter.
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
